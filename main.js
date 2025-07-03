5d619d9db81eb6cad2c041165ace856ee3a48342
Ammoknoxxed <ammoknoxx@protonmail.com>
2025-07-03 20:17:08 +0200

    Deine Nachricht: z. B. Add GitHub Actions workflow

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow }     = require('electron');
+const { autoUpdater }            = require('electron-updater');
+const path                       = require('path');
+
+let win;
 
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: path.join(__dirname, 'icon.ico')  // ← Hier wird das Icon gesetzt
+    icon: path.join(__dirname, 'icon.ico')
   });
-
   win.loadFile('Freespins Logger.html');
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+  // prüft auf Updates und zeigt Notification an
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+// (Optional) Events für Statusmeldungen
+autoUpdater.on('update-available', () => {
+  win.webContents.send('update_available');
+});
+autoUpdater.on('update-downloaded', () => {
+  win.webContents.send('update_downloaded');
+});
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
